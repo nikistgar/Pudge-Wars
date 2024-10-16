cc8f3746ab3167a5e0e1d7a02554634d43f83189
nikistgar <nikistgar@gmail.com>
2024-10-16 22:23:26 +0300

    OpenTK

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+
+namespace PudgeWars
+{
+    class Main : GameWindow
+    {
+        public Main(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings()
+        {
+            Title = title,
+            Size = new Vector2i(width, height),
+            WindowBorder = WindowBorder.Resizable,
+            StartVisible = false,
+            StartFocused = true,
+            WindowState = WindowState.Normal,
+            API = ContextAPI.OpenGL,
+            Profile = ContextProfile.Core,
+            APIVersion = new Version(3, 3),
+        })
+        {
+            CenterWindow();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad()
+        {
+            IsVisible = true;
+
+            base.OnLoad();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PudgeWars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using Main window = new Main(1920, 720, "Baza");
+            window.Run();
+        }
+    }
+}
